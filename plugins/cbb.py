f9025b16ffa87d97e55335f00a840bcf94246224
Fahriong <105623760+Fahriong@users.noreply.github.com>
2022-05-16 12:33:09 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/fahriwetv08/filesharelinkpreset'>Click here</a>\n○ Channel : @presetrend\n○ Support Channel : @playlistrend</b>",
+            text = f"<b>○ Powerd by : <a href='tg://user?id={OWNER_ID}'>ORANG INI</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://youtube.com/c/Presetrend'>TEKAN INI</a>\n○ Channel : @presetking\n○ Support Channel : @presetboomz</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
